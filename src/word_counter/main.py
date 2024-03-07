778dc3fc715ba408fd458e013f1fb7afeb53b514
Elan Deyan <deyancaio83@hotmail.com>
2024-03-07 20:43:24 -0300

    feat: adds lines, words commands to cli

--- src/word_counter/main.py
+++ src/word_counter/main.py
@@ -1,6 +1,8 @@
 import click
 
-from src.word_counter.commands.size.size import size
+from src.word_counter.commands.lines.command import lines
+from src.word_counter.commands.size.command import size
+from src.word_counter.commands.words.command import words
 
 
 @click.group
@@ -9,6 +11,8 @@ def cli():
 
 
 cli.add_command(size)
+cli.add_command(lines)
+cli.add_command(words)
 
 
 if __name__ == "__main__":
