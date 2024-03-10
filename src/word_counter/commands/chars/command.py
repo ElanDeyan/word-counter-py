ad3bc78e160a45e13228f1f7ee4781862098319a
Elan Deyan <elandeyandasilva@gmail.com>
2024-03-10 19:44:53 -0300

    feat(chars): adds output format and variadic arguments

--- src/word_counter/commands/chars/command.py
+++ src/word_counter/commands/chars/command.py
@@ -2,15 +2,30 @@ import os
 from pathlib import Path
 import click
 
+from src.word_counter.services.options.format.format_type_from_str import format_type_from_str
+from src.word_counter.services.options.format.output_formatter import output_formatter
+
 
 @click.command
+@click.pass_context
 @click.argument(
-    "filepath", type=click.Path(exists=True, path_type=Path, file_okay=True)
+    "files", type=click.Path(exists=True, path_type=Path, file_okay=True),
+    nargs=-1
 )
 @click.option(
     "--ignore-line-sep", is_flag=True, default=False, show_default=True, type=bool
 )
-def chars(filepath: Path, ignore_line_sep: bool):
-    with open(filepath.name, "r", encoding="utf-8") as file:
-        content = file.read().strip(os.linesep) if ignore_line_sep else file.read()
-        click.echo(f"{{filename: {filepath.name}, chars: {len(content)}}}")
+def chars(ctx: click.Context, files: list[Path], ignore_line_sep: bool):
+    output_format: str = ctx.obj["format"]
+    format_type = format_type_from_str(output_format)
+
+    files_and_chars: list[dict[str, object]] = []
+
+    for filepath in files:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            content = f.read().strip(os.linesep) if ignore_line_sep else f.read()
+            files_and_chars.append({ "filepath": filepath.as_posix(), "char_count": len(content) })
+    
+    data = output_formatter(files_and_chars, format_type)
+
+    click.echo(data)
