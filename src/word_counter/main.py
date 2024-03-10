db107d100416c82487681d6476f6266e2d5b34c0
Elan Deyan <elandeyandasilva@gmail.com>
2024-03-10 19:43:13 -0300

    feat(chars): adds chars command

--- src/word_counter/main.py
+++ src/word_counter/main.py
@@ -1,5 +1,6 @@
 import click
 
+from src.word_counter.commands.chars.command import chars
 from src.word_counter.commands.lines.command import lines
 from src.word_counter.commands.size.command import size
 from src.word_counter.options.format.OutputFormats import OutputFormats
@@ -23,6 +24,7 @@ def cli(output_format: str):
 cli.add_command(size)
 cli.add_command(lines)
 cli.add_command(words)
+cli.add_command(chars)
 
 
 if __name__ == "__main__":
