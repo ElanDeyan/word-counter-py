efe50761da4851ba72796d1dc840b5468ca30027
Elan Deyan <elandeyandasilva@gmail.com>
2024-03-10 19:16:16 -0300

    feat(words): adds variadic number of files and output format

--- src/word_counter/commands/words/command.py
+++ src/word_counter/commands/words/command.py
@@ -1,12 +1,27 @@
 from pathlib import Path
 import click
 
+from src.word_counter.services.options.format.format_type_from_str import (
+    format_type_from_str,
+)
+from src.word_counter.services.options.format.output_formatter import output_formatter
+
 
 @click.command
+@click.pass_context
 @click.argument(
-    "filepath", type=click.Path(exists=True, path_type=Path, file_okay=True)
+    "files", type=click.Path(exists=True, path_type=Path, file_okay=True), nargs=-1
 )
-def words(filepath: Path):
-    with open(filepath, "r") as file:
-        words = file.read().split()
-        click.echo(f"{{filename: {filepath.name}, words: {len(words)}}}")
+def words(ctx: click.Context, files: list[Path]):
+    output_format: str = ctx.obj["format"]
+    format_type = format_type_from_str(output_format)
+    files_and_words: list[dict[str, object]] = []
+
+    for file in files:
+        with open(file, "r") as f:
+            words_list = f.read().split()
+            files_and_words.append({"file": file.name, "words_count": len(words_list)})
+
+    data = output_formatter(files_and_words, format_type)
+
+    click.echo(data)
