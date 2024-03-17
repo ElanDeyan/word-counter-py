9de6e28f6768cd70189e780ddb15828d2fd8b20d
Elan Deyan <elandeyandasilva@gmail.com>
2024-03-16 23:26:14 -0300

    fix: fixes logic for conversion between bytes to other formats

--- src/word_counter/services/commands/size/size_from_bytes.py
+++ src/word_counter/services/commands/size/size_from_bytes.py
@@ -2,17 +2,14 @@ from src.word_counter.commands.size.SizeUnits import SizeUnit
 
 
 def size_from_bytes(size_in_bytes: int, output_unit: SizeUnit) -> float:
-    size: float
+    if output_unit is SizeUnit.BYTES:
+        return size_in_bytes
 
-    match output_unit:
-        case SizeUnit.BYTES:
-            size = float(size_in_bytes)
-        case SizeUnit.KILOBYTES:
-            size = size_in_bytes / 1e3
-        case SizeUnit.MEGABYTES:
-            size = size_in_bytes / 1e6
-        case SizeUnit.GIGABYTES:
-            size = size_in_bytes / 1e9
-        case SizeUnit.TERABYTES:
-            size = size_in_bytes / 1e12
-    return size
+    unit_index_map: dict[str, int] = {
+        unit: index
+        for unit, index in zip(SizeUnit.values(), range(len(SizeUnit.values())))
+    }
+
+    conversion_factor = 1024 ** unit_index_map[output_unit.value]
+
+    return round(size_in_bytes / conversion_factor, 4)
