8ed743b01aff546d2e1d374f7585cf21afcadcd2
Elan Deyan Almeida <elandeyandasilva@gmail.com>
2024-03-22 23:36:06 -0300

    feat: adds support to read stdin and removes format option from the entry point
    Format option now is available individually in each command

--- src/word_counter/main.py
+++ src/word_counter/main.py
@@ -1,3 +1,4 @@
+import sys
 import click
 
 from src.word_counter.commands.scan.command import scan
@@ -5,20 +6,15 @@ from src.word_counter.commands.chars.command import chars
 from src.word_counter.commands.lines.command import lines
 from src.word_counter.commands.size.command import size
 from src.word_counter.commands.words.command import words
-from src.word_counter.options.format.OutputFormats import OutputFormats
 
 
 @click.group
-@click.option(
-    "--format",
-    "output_format",
-    type=click.Choice(OutputFormats.values(), case_sensitive=False),
-    default=OutputFormats.PLAINTEXT.value,
-    show_default=True,
-    prompt="Choose a format",
-)
-def cli(output_format: str):
-    click.get_current_context().obj = {"format": output_format}
+def cli():
+    click.get_current_context().obj = {"stdin": None}
+
+    if not sys.stdin.isatty():
+        stdin_output = sys.stdin
+        click.get_current_context().obj["stdin"] = stdin_output
 
 
 cli.add_command(size)
