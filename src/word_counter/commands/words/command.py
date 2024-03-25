3ea3260dd41437c4503fe369de623be1ab05af4c
Elan Deyan Almeida <elandeyandasilva@gmail.com>
2024-03-25 19:06:08 -0300

    docs: adds docs for the words command

--- src/word_counter/commands/words/command.py
+++ src/word_counter/commands/words/command.py
@@ -16,6 +16,9 @@ from src.word_counter.services.options.format.output_formatter import output_for
 )
 @common_options
 def words(ctx: click.Context, files: list[Path], output_format: str):
+    """
+    Counts the number of words in each input from FILES, and formats the output based on the specified format type.
+    """
     format_type = format_type_from_str(output_format)
     files_and_words: list[dict[str, object]] = []
 
