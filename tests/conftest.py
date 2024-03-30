6684e193d1c75489993888a54ee1a88460009dae
Elan Deyan Almeida <elandeyandasilva@gmail.com>
2024-03-30 18:43:44 -0300

    test: adds tests for filesize core function

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+from src.word_counter.utils.SizeUnits import SizeUnit
+
+
+def convert_size(
+    size_in_bytes: int, target_unit: SizeUnit, decimal_precision: int = 2
+) -> float:
+    """
+    The function `convert_size` converts a size in bytes to a specified unit with a specified decimal
+    precision.
+
+    :param size_in_bytes: The `size_in_bytes` parameter represents the size in bytes that you want to
+    convert to a different unit of size
+    :type size_in_bytes: int
+    :param target_unit: The `target_unit` parameter in the `convert_size` function is of type
+    `SizeUnit`. It is used to specify the unit to which the size in bytes should be converted. The
+    `SizeUnit` is likely an enumeration or a custom class that defines different units such as bytes,
+    kiloby
+    :type target_unit: SizeUnit
+    :param decimal_precision: The `decimal_precision` parameter in the `convert_size` function specifies
+    the number of decimal places to round the converted size to. This parameter determines the level of
+    precision in the output of the size conversion
+    :type decimal_precision: int
+    :return: the size in the specified target unit with the specified decimal precision.
+    """
+    return round(
+        size_in_bytes / (target_unit.value.conversion_from_bytes_factor),
+        decimal_precision,
+    )
--- tests/core/test_filesize.py
+++ tests/core/test_filesize.py
@@ -4,6 +4,7 @@ import pytest
 
 from src.word_counter.core.filesize import filesize
 from src.word_counter.utils.SizeUnits import SizeUnit
+from tests.conftest import convert_size
 from tests.constants import EXAMPLE_DIRECTORY, TEST_FILE_NAME
 
 
@@ -17,5 +18,19 @@ def test_file_size_in_bytes(test_file: Path):
     return os.path.getsize(test_file)
 
 
+@pytest.fixture
+def size_units_values():
+    return SizeUnit._member_map_.values()
+
+
 def test_size_in_bytes(test_file_size_in_bytes: int, test_file: Path):
     assert f"{test_file_size_in_bytes}" in filesize(test_file, SizeUnit.BYTES)
+
+
+def test_size_all_units(
+    test_file: Path, test_file_size_in_bytes: int, size_units_values: list[SizeUnit]
+):
+    for size_unit in size_units_values:
+        converted_size = convert_size(test_file_size_in_bytes, size_unit)
+
+        assert f"{converted_size}" in filesize(test_file, size_unit)
